877152caa0b52e3f45e416e167b128210577f839
leedayoung <leedayoung0601@gmail.com>
2023-11-14 18:02:12 +0900

    뒤로가기/홈버튼 구현

--- app/src/main/java/com/example/farmdoctor/MainActivity.java
+++ app/src/main/java/com/example/farmdoctor/MainActivity.java
@@ -1,25 +1,28 @@
 package com.example.farmdoctor;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.farmdoctor.R;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // 'activity_main.xml' 레이아웃을 표시
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "onCreate");
 
         // '제철농산물' 버튼 클릭 이벤트 처리
         Button button5 = findViewById(R.id.button5);
         button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 'month.xml' 레이아웃을 표시
-                setContentView(R.layout.month);
+                // 'MonthActivity'를 시작하도록 변경
+                Intent intent = new Intent(MainActivity.this, MonthActivity.class);
+                startActivity(intent);
             }
         });
     }
