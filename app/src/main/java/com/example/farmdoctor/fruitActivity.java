1d6a5f3007812b1a2fc907d917f111a1b8dbe7d2
Lee Jeongyeon <ljy5786@gmail.com>
2023-11-17 04:03:36 +0900

    가이드 페이지 만들기, 과일류>버튼1>가이드페이지 연결

--- app/src/main/java/com/example/farmdoctor/fruitActivity.java
+++ app/src/main/java/com/example/farmdoctor/fruitActivity.java
@@ -2,6 +2,7 @@ package com.example.farmdoctor;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -33,5 +34,17 @@ public class fruitActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // 'button1' 버튼 클릭 이벤트 처리
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 'button1'을 시작하도록 변경
+                Intent intent = new Intent(fruitActivity.this, guideActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
