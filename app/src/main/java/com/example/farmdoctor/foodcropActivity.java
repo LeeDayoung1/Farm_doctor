506d6c876a3cdc371c127abb615f498cffbe1f06
Lee Jeongyeon <ljy5786@gmail.com>
2023-11-24 17:18:36 +0900

    부류별 버튼 생성

--- app/src/main/java/com/example/farmdoctor/foodcropActivity.java
+++ app/src/main/java/com/example/farmdoctor/foodcropActivity.java
@@ -1,6 +1,7 @@
 package com.example.farmdoctor;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -8,19 +9,24 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class foodcropActivity extends AppCompatActivity {
     public static String keyItemCode, keyKindCode, keyRanks, keyImage;
 
-    String [] fcItemCodeList = {"0","152","152","152","152","152","152","152","151","161","143","143","144","162","163","111","111","164","112","141","141","141","142","142"};
+    String [] fcItemCodeList = {"_","152","152","152","152","152","152","152","152","152","152","152","152","152","152","151","151",
+            "161","161","143","143","143","144","162","162","163", "163","111","111","111","111","164","164","112","112","141","141",
+            "141","141","141","142","142","142"};
 
-    String [] fcKindCodeList = {"0","1","2","3","4","5","6","0","1","0","1","1","1","1","1","5","1","1","1","3","2","0","1"};
-    String [] fcRanksList = {"상품","중품","상품","중품","상품","중품","상품","중품","상품","중품","상품","중품","상품","중품","상품","중품","상품","중품","상품","중품","중품","중품","상품","중품",
-            "상품","중품","상품","중품","상품","중품","상품","중품","상품","중품","상품","중품","중품","상품","중품","상품","중품","중품"};
-    String [] fcImageList = {"potato","potato","potato","potato","potato","potato","potato","sweetpotato","oat","greenbean","greenbean","buckwheat","barley","sorghumbicolor",
-            "rice","rice","jobstears","glutinousrice","beans","beans","beansprouts","redbeans","redbeans"};
+    String [] fcKindCodeList = {"_","00","00","01","01","02","02","03","03","04","04","05","05","06","06","00","00","01","01","00","00",
+            "01","01","01","01","01","01","01","01","05","05", "01","01","01","01","01","01","03","02","02","00","00","01"};
+    String [] fcRanksList = {"_", "상품","중품","상품","중품","상품","중품","상품","중품","상품","중품","상품","중품","상품","중품","상품","중품",
+            "상품","중품","상품","중품","중품","중품","상품","중품","상품","중품","상품","중품","상품","중품","상품","중품","상품","중품","상품","중품",
+            "중품","상품","중품","상품","중품","중품"
+    };
+    String [] fcImageList = {"_", "potato","potato","potato","potato","potato","potato","potato","potato","potato","potato","potato",
+            "potato","potato","potato","sweetpotato","sweetpotato","oat","oat","greenbean","greenbean","greenbean","buckwheat","barley",
+            "barley","sorghumbicolor","sorghumbicolor","rice","rice","rice","rice","jobstears","jobstears","glutinousrice",
+            "glutinousrice","beans","beans","beans","beansprouts","beansprouts","redbeans","redbeans","redbeans"};
 
 
     @Override
@@ -50,16 +56,40 @@ public class foodcropActivity extends AppCompatActivity {
             }
         });
 
-
-        // 'button1' 버튼 클릭 이벤트 처리
+        // 버튼들을 생성하고 클릭 이벤트 처리 함수 호출
+        createButtons();
+/*        // 'button1' 버튼 클릭 이벤트 처리
         Button button1 = findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                keyItemCode = fcItemCodeList[1];
-                keyKindCode = fcKindCodeList[1];
-                keyRanks = fcRanksList[1];
-                keyImage = fcImageList[1];
+                keyItemCode = fcItemCodeList[2];
+                keyKindCode = fcKindCodeList[2];
+                keyRanks = fcRanksList[2];
+                keyImage = fcImageList[2];
+                Intent intent = new Intent(foodcropActivity.this, guideFoodcropActivity.class);
+                startActivity(intent);
+            }
+        });*/
+    }
+
+    // 버튼 생성 및 클릭 이벤트 처리 함수
+    private void createButtons() {
+        for (int i = 1; i <= 42; i++) {
+            createButton(i);
+        }
+    }
+
+    // 각 버튼의 생성 및 클릭 이벤트 처리 함수
+    private void createButton(final int index) {
+        Button button = findViewById(getResources().getIdentifier("button" + index, "id", getPackageName()));
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                keyItemCode = fcItemCodeList[index];
+                keyKindCode = fcKindCodeList[index];
+                keyRanks = fcRanksList[index];
+                keyImage = fcImageList[index];
                 Intent intent = new Intent(foodcropActivity.this, guideFoodcropActivity.class);
                 startActivity(intent);
             }
